519d3fc94ab9ab9be30c9d5b583aac22011f1526
Abdulqadir8604 <web.aq8604@gmail.com>
2024-08-03 18:24:50 +0530

    fixed post drop a commit

--- lib/pages/myaccount.dart
+++ lib/pages/myaccount.dart
@@ -1,11 +1,17 @@
 import 'dart:developer';
+import 'dart:io';
+import 'dart:typed_data';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:m_worker/bloc/theme_bloc.dart';
 import 'package:m_worker/components/button.dart';
 import 'package:m_worker/utils/api.dart';
+import 'package:s3_storage/s3_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MyAccount extends StatefulWidget {
@@ -17,13 +23,19 @@ class MyAccount extends StatefulWidget {
 
 class _MyAccountState extends State<MyAccount> {
   late bool showWeather = false;
+  late bool biometricsEnabled = false;
+
+  late var _companyName = '';
+  late var _pfp = '';
 
   final _workerData = {};
+  final _auth = LocalAuthentication();
 
   @override
   void initState() {
     _fetchPrefs();
     _fetchWorkerData();
+    _getPfp();
     super.initState();
   }
 
@@ -34,19 +46,118 @@ class _MyAccountState extends State<MyAccount> {
     setState(() {
       _workerData.addAll(res['data'][0]);
     });
+    log('Worker data: $_workerData');
   }
 
   void _fetchPrefs() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
+      _companyName = prefs.getString('company') ?? '';
       showWeather = prefs.getBool('showWeather') ?? true;
+      biometricsEnabled = prefs.getBool('biometricsEnabled') ?? false;
     });
   }
 
-  void _savePrefs(bool value) async {
+  void _savePrefs(bool value, type) async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.setBool('showWeather', value);
+    if (type == 'showWeather') {
+      prefs.setBool('showWeather', value);
+    } else {
+      prefs.setBool('biometricsEnabled', value);
+    }
   }
+
+  Future<void> _authenticate() async {
+    try {
+      final availableBiometrics = await _auth.getAvailableBiometrics();
+      if (availableBiometrics.isEmpty) {
+        log('Biometric authentication not available.');
+        return;
+      }
+      log('Available biometrics: $availableBiometrics');
+      final canCheckBiometrics = await _auth.canCheckBiometrics;
+      if (!canCheckBiometrics) {
+        log('Biometric authentication not available.');
+        return;
+      }
+      final isAuthenticated = await _auth.authenticate(
+        localizedReason: 'Authenticate to enable biometrics',
+        options: const AuthenticationOptions(
+          stickyAuth: true,
+          useErrorDialogs: true,
+        ),
+      );
+      if (isAuthenticated) {
+        final prefs = await SharedPreferences.getInstance();
+        prefs.setBool('biometricsEnabled', biometricsEnabled);
+        log('Authenticated');
+      } else {
+        log('Not authenticated');
+      }
+    } catch (e) {
+      log('Error authenticating: $e');
+    }
+  }
+
+  void _uploadDoc(image) async {
+
+    try {
+      final s3Storage = S3Storage(
+        endPoint: 's3.ap-southeast-2.amazonaws.com',
+        accessKey: dotenv.env['accessKey']!,
+        secretKey: dotenv.env['secretKey']!,
+        region: 'ap-southeast-2',
+      );
+
+      const storage = FlutterSecureStorage();
+      final company = await storage.read(key: 'company');
+      final email = await storage.read(key: 'email');
+
+      await s3Storage.putObject(
+        'moscaresolutions',
+        '$company/worker/$email/profile_picture/pfp.${image.path.split('/').last.toString().split('.').last.toString()}',
+          Stream<Uint8List>.value(Uint8List.fromList(image.readAsBytesSync())),
+        onProgress: (progress) {
+          log('Progress: $progress');
+        },
+      );
+
+    } catch (e) {
+      log('Error uploading document: $e');
+    } finally {
+      Navigator.pop(context);
+      _getPfp();
+    }
+  }
+
+  void _getPfp() async {
+    try {
+      final s3Storage = S3Storage(
+        endPoint: 's3.ap-southeast-2.amazonaws.com',
+        accessKey: dotenv.env['accessKey']!,
+        secretKey: dotenv.env['secretKey']!,
+        region: 'ap-southeast-2',
+      );
+
+      const storage = FlutterSecureStorage();
+      final company = await storage.read(key: 'company');
+      final email = await storage.read(key: 'email');
+
+      final url = await s3Storage.presignedGetObject(
+        'moscaresolutions',
+        '$company/worker/$email/profile_picture/pfp.jpg',
+      );
+      log('URL: $url');
+
+      setState(() {
+        _pfp = url;
+      });
+
+    } catch (e) {
+      log('Error getting profile picture: $e');
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -64,11 +175,44 @@ class _MyAccountState extends State<MyAccount> {
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 const SizedBox(height: 20),
-                CircleAvatar(
-                  radius: 50,
-                  backgroundColor: colorScheme.primary,
-                  child: Icon(Icons.person,
-                      size: 50, color: colorScheme.onPrimary),
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: colorScheme.secondaryContainer,
+                      child: _pfp.isNotEmpty
+                          ? Image.network(
+                        _pfp, // Use the image data
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      )
+                          : Icon(
+                        Icons.person,
+                        size: 50,
+                        color: colorScheme.primary,
+                      ),
+                    ),
+
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: CircleAvatar(
+                        radius: 15,
+                        backgroundColor: colorScheme.primary,
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.edit,
+                            size: 15,
+                            color: colorScheme.onPrimary,
+                          ),
+                          onPressed: () {
+                            _showPfpDialog(colorScheme, context);
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 const SizedBox(height: 20),
                 Text(
@@ -80,25 +224,40 @@ class _MyAccountState extends State<MyAccount> {
                 ),
                 const SizedBox(height: 10),
                 Text(
-                  'Company Name',
+                  _companyName.toString().toUpperCase() ?? '',
                   style: TextStyle(
                     fontSize: 16,
                     color: colorScheme.primary,
                   ),
                 ),
                 const SizedBox(height: 30),
-                MButton(
-                  colorScheme: colorScheme,
-                  label: 'Change Profile Picture',
-                  onPressed: () {},
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Card(
+                    color: colorScheme.secondaryContainer,
+                    child: ListTile(
+                      title: Text(
+                        'Biometrics',
+                        style: TextStyle(color: colorScheme.onSecondaryContainer, fontSize: 14),
+                      ),
+                      trailing: Switch(
+                        value: biometricsEnabled,
+                        onChanged: (value) {
+                          setState(() {
+                            biometricsEnabled = value;
+                            if (value) {
+                              _authenticate();
+                            } else {
+                              _savePrefs(value,'biometricsEnabled');
+                            }
+                          });
+                        },
+                        activeColor: colorScheme.primary,
+                      ),
+                    ),
+                  ),
                 ),
-                const SizedBox(height: 10),
-                MButton(
-                  colorScheme: colorScheme,
-                  label: 'Enable Biometrics',
-                  onPressed: () {},
-                ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 5),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 50),
                   child: Card(
@@ -113,7 +272,7 @@ class _MyAccountState extends State<MyAccount> {
                         onChanged: (value) {
                           setState(() {
                             showWeather = value;
-                            _savePrefs(value);
+                            _savePrefs(value, 'showWeather');
                           });
                         },
                         activeColor: colorScheme.primary,
@@ -131,6 +290,85 @@ class _MyAccountState extends State<MyAccount> {
               ],
             ),
           ),
+        );
+      },
+    );
+  }
+
+  void _showPfpDialog(ColorScheme colorScheme, BuildContext context) {
+    XFile? _image;
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.close, color: colorScheme.primary),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.check, color: colorScheme.primary),
+                        onPressed: () async {
+                          _uploadDoc(File(_image!.path));
+                        },
+                      ),
+                    ],
+                  ),
+                  _image != null
+                      ? Image.file(File(_image!.path), width: 150, height: 150)
+                      : const SizedBox.shrink(),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Change Profile Picture',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: colorScheme.primary,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  MButton(
+                    label: 'Take a photo',
+                    onPressed: () async {
+                      final ImagePicker _picker = ImagePicker();
+                      final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+                      if (image != null) {
+                        setState(() {
+                          _image = image;
+                        });
+                      }
+                    },
+                    colorScheme: colorScheme,
+                  ),
+                  const SizedBox(height: 10),
+                  MButton(
+                    label: 'Choose from gallery',
+                    onPressed: () async {
+                      final ImagePicker _picker = ImagePicker();
+                      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+                      if (image != null) {
+                        setState(() {
+                          _image = image;
+                        });
+                      }
+                    },
+                    colorScheme: colorScheme,
+                  ),
+                ],
+              ),
+            );
+          },
         );
       },
     );
